66d8d394628ab52681baa0da3bf46863f95e3eb3
Celso Martins <celso.av.martins@gmail.com>
2024-11-11 18:57:44 -0300

    feat: login with rails 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  include Authentication
+
+  allow_unauthenticated_access only: [ :new, :create ]
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  allow_unauthenticated_access only: [ :index ]
+
+  def index
+  end
+
+  def dashboard
+  end
+end
